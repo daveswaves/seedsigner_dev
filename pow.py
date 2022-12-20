6bd36707a3abd90d92fec12d80e55bf4ca3891a3
David <daveswaves.2008@gmail.com>
2022-12-20 16:19:29 +0000

    Added grep & pow files

---
+++ pow.py
@@ -0,0 +1,12 @@
+import hashlib
+# import embit
+# from embit import bip39
+
+for i in range(0, 100000000):
+    hash_fmt = hashlib.sha256( str(i).encode() ).hexdigest()
+    
+    if '0000000' == hash_fmt[0:7]:
+        print(hash_fmt)
+    
+    # binary_fmt = "{0:08b}".format(int(hash_fmt, 16))
+    # print(hash_fmt, str(binary_fmt))
